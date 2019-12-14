46cf06e0858335756c3a40a4f7e33bd9d93df78e
melvinlee <melvinlee@hotmail.com>
2019-12-14 11:43:01 +0800

    refactored sample code.

--- examples/weavescope/main.tf
+++ examples/weavescope/main.tf
@@ -1,7 +1,7 @@
 
 provider "helm" {
   version = "<= 0.10.4"
-  
+
   kubernetes {
     config_path = "~/.kube/config"
   }
@@ -11,9 +11,29 @@ provider "helm" {
   install_tiller  = false
 }
 
+locals {
+  namespace = "monitoring"
+}
+
+resource "kubernetes_namespace" "weavescope_namespace" {
+  metadata {
+    name = local.namespace
+  }
+}
+
 module "weavescope" {
-    source = "../../"
-    helm_release_name = "weave-scope"
-    namespace = "monitoring"
-    chart_version = "1.1.8"
+  source             = "../../"
+  helm_release_name  = "weave-scope"
+  helm_namespace     = local.namespace
+  helm_chart_version = "1.1.8"
+  dependencies       = ["kubernetes_namespace.weavescope_namespace"]
+}
+
+resource "null_resource" "helm_list" {
+  depends_on = [module.weavescope.depended_on]
+
+  provisioner "local-exec" {
+    command = "helm list --namespace ${local.namespace}"
+  }
+
 }
